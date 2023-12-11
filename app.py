1a806d59ec6da7c40072c8a08af4cf4cf8b02a00
Kishan <payadikishan@gmail.com>
2023-12-11 18:58:41 +0530

    Basic template created

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# Routes
-
-@app.route("/")
-def home():
+@app.route('/')
+def index():
     return render_template('form.html')
 
-@app.route("/resume", methods=["GET", "POST"])
-def CreateResume():
-    name = request.form
-    return render_template('resume.html', name = name)
-
-# Execution of main code
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return render_template('resume.html', name=name, email=email)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
