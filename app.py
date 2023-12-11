b18a870a5031c351d2f51dcb08f53c8c27d7c5fa
Kishan <payadikishan@gmail.com>
2023-12-11 19:24:00 +0530

    Some more blocks of informations added

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template('form.html')
 
+
+
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        return render_template('resume.html', name=name, email=email)
+        education = request.form['education']
+        experience = request.form['experience']
+        return render_template('resume.html', name=name, email=email, education=education, experience=experience)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
